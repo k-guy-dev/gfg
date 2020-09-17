ed5f6195cd393b5e9a8a9fc3864e4cd118d6280f
Pratham Bhargava <pratham.bhargava2018@vitbhopal.ac.in>
2020-09-17 14:31:01 +0530

    prac

---
+++ arrays/transitionPoint.java
@@ -0,0 +1,42 @@
+/**
+ * You are given a sorted array containing only numbers 0 and 1. Find the transition point efficiently. The transition point is a point where "0" ends and "1" begins (0 based indexing).
+Note that, if there is no "1" exists, print -1.
+Note that, in case of all 1s print 0.
+
+
+ */
+
+package arrays;
+
+// { Driver Code Starts
+import java.util.*;
+
+class Sorted_Array {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            GfG obj = new GfG();
+            System.out.println(obj.transitionPoint(arr, n));
+            T--;
+        }
+    }
+}// } Driver Code Ends
+
+class GfG {
+    int transitionPoint(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                return i;
+            }
+
+        }
+        return -1;
+
+    }
+}
