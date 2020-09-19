8f3b2c946a4ac3ffc19a5e01f550a384c1c36ae1
Pratham Bhargava <pratham.bhargava2018@vitbhopal.ac.in>
2020-09-20 00:25:01 +0530

    pairWithGivenSum

---
+++ arrays/arrayoperations/findingPairWithAGivenSum.java
@@ -0,0 +1,26 @@
+package arrays.arrayoperations;
+
+class findingPairWithAGivenSum {
+    static boolean pairSumPresent(int arr[], int n, int x) {
+        int i;
+        // Finding Pivot Element
+        for (i = 0; i < n; i++) {
+            if (arr[i] > arr[i + 1])
+                break;
+        }
+        int l = (i + 1) % n; // l being the index of smallest element
+        int r = i; // r being the index of largest element
+
+        while (l != r) {
+            if (arr[l] + arr[r] == x) {
+                return true;
+            }
+            if (arr[l] + arr[r] < x) {
+                l = (l + 1) % r;
+            } else {
+                r = (n + r - 1) % n;
+            }
+        }
+        return false;
+    }
+}
